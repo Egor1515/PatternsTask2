58c3531cc1ffc1816551a34d75746e6e58e16b36
Egor1515 <96645276+Egor1515@users.noreply.github.com>
2022-04-27 08:18:49 +1000

    CardTests

--- src/test/java/TestMode.java
+++ src/test/java/TestMode.java
@@ -1,41 +1,73 @@
-import com.github.javafaker.Faker;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.http.ContentType;
+import io.restassured.internal.RequestSpecificationImpl;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import ru.netology.testmode.data.DataGenerator;
 
-import java.util.Locale;
-
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.restassured.RestAssured.given;
+import static ru.netology.testmode.data.DataGenerator.Registration.getRegisteredUser;
+import static ru.netology.testmode.data.DataGenerator.Registration.getUser;
+import static ru.netology.testmode.data.DataGenerator.getRandomLogin;
+import static ru.netology.testmode.data.DataGenerator.getRandomPassword;
+
+class AuthTest {
 
 
-public class TestMode {
-    Faker faker = new Faker(new Locale("ru"));
-
-
-    private final static RequestSpecification requestSpec = new RequestSpecBuilder()
-            .setBaseUri("http://localhost")
-            .setPort(9999)
-            .setAccept(ContentType.JSON)
-            .setContentType(ContentType.JSON)
-            .log(LogDetail.ALL)
-            .build();
-
-    @Test
-
-    void setUpAll() {
-
-        given()
-                .spec(requestSpec)
-                .body(new RegistrationInfo(faker.name().fullName(), faker.numerify("###"),faker.expression("active")))
-                .when()
-                .post("/api/system/users")
-                .then()
-                .statusCode(200);
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
     }
 
+    @Test
+    @DisplayName("Should successfully login with active registered user")
+    void shouldSuccessfulLoginIfRegisteredActiveUser() {
 
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue("vasya");
+        $("[data-test-id='password'] input").setValue("password");
+        $(".button[role='button']").click();
+
+
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser() {
+        var notRegisteredUser = getUser("active");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет
+        //  незарегистрированного пользователя, для заполнения полей формы используйте пользователя notRegisteredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser() {
+        var blockedUser = getRegisteredUser("blocked");
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет,
+        //  заблокированного пользователя, для заполнения полей формы используйте пользователя blockedUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongLogin = getRandomLogin();
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  логином, для заполнения поля формы "Логин" используйте переменную wrongLogin,
+        //  "Пароль" - пользователя registeredUser
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword() {
+        var registeredUser = getRegisteredUser("active");
+        var wrongPassword = getRandomPassword();
+        // TODO: добавить логику теста в рамках которого будет выполнена попытка входа в личный кабинет с неверным
+        //  паролем, для заполнения поля формы "Логин" используйте пользователя registeredUser,
+        //  "Пароль" - переменную wrongPassword
+    }
 }
-
---
+++ src/test/java/ru/netology/testmode/data/DataGenerator.java
@@ -0,0 +1,79 @@
+package ru.netology.testmode.data;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto user) {
+        given()
+                .spec(requestSpec)
+                .body(new RegistrationDto("vasya", "123123", "active"));
+        when()
+                .post("api/system/users")
+                .then()
+                .statusCode(200);
+
+    }
+
+    public static String getRandomLogin() {
+        var login = faker.funnyName().name();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        var password = faker.numerify("#####");
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getUser(String status) {
+            return new RegistrationDto(getRandomLogin(), getRandomPassword(), "active");
+
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            var registeredUser = getUser("active");
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+        public static RegistrationDto notRegisteredUser (String status) {
+            var notRegisteredUser = getUser("active");
+return notRegisteredUser;
+        }
+        public static RegistrationDto blockedUser (String status) {
+            var notRegisteredUser = getUser("blocked");
+return notRegisteredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
